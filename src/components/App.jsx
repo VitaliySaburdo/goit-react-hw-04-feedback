fca59c7b3e17c71b23553cf117f5745933fcd9d7
VitaliySaburdo <vsaburdo@gmail.com>
2023-01-25 23:18:39 +0200

    12

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,9 +3,9 @@ import { Statistics } from './Statistics/Statistics';
 import { Section } from './Section/Section';
 import { Notification } from './Notification/Notification';
 // import PropTypes from 'prop-types';
-import {  useState } from 'react';
+import { useState } from 'react';
 
-export function App () {
+export function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
@@ -22,37 +22,43 @@ export function App () {
         setBad(state => state + 1);
         break;
       default:
-        return
+        return;
     }
-  }
-    
-    const total = bad + neutral + good;
-    const positivePercentage = Math.ceil((good / total) * 100);
-    const options = Object.keys({good, neutral, bad});
+  };
+  const countTotalFeedback = () => {
+    return bad + neutral + good;
+  };
+  const countPositiveFeedbackPercentage = () => {
+    const total = countTotalFeedback();
+    return Math.ceil((good / total) * 100);
+  };
+
+  const positivePercentage = countPositiveFeedbackPercentage();
+  const total = countTotalFeedback();
+  const options = Object.keys({ good, neutral, bad });
 
   return (
-      <>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={options}
-            onLeaveFeedback={handleIncrement}
-          ></FeedbackOptions>
-        </Section>
+    <>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={options}
+          onLeaveFeedback={handleIncrement}
+        ></FeedbackOptions>
+      </Section>
 
-        {total === 0 ? (
-          <Notification message="There is no feedback" />
-        ) : (
-          <Section title="Statistics">
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={total}
-              positivePercentage={positivePercentage}
-            ></Statistics>
-          </Section>
-        )}
-      </>
-    );
-  
+      {total === 0 ? (
+        <Notification message="There is no feedback" />
+      ) : (
+        <Section title="Statistics">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={positivePercentage}
+          ></Statistics>
+        </Section>
+      )}
+    </>
+  );
 }
